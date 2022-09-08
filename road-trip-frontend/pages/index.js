9482c4ab645dd03d4448d607c87dc08976881082
Austin Huizinga <austinth127@gmail.com>
2022-09-08 15:46:27 -0500

    text

--- road-trip-frontend/pages/index.js
+++ road-trip-frontend/pages/index.js
@@ -2,8 +2,10 @@ export default function Home() {
     return (
         <>
             <div className="flex flex-col h-screen justify-center items-center">
-                <h1 className="text-8xl text-center font-bold uppercase">
-                    Road Trip <br /> planner thing
+                <h1 className="text-6xl text-center font-bold uppercase">
+                    Road Trip Planner
+                    <br />
+                    (name pending)
                 </h1>
                 <h3 className="text-center font-light text-gray-300">
                     Brought to you by Boothverse 2: Electric Bogaloo
