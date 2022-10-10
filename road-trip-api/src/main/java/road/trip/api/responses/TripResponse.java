0bc5f890665704021b1feb8303b11c11bed4fd08
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2022-10-10 13:09:12 -0500

    Update TripResponse.java

--- road-trip-api/src/main/java/road/trip/api/responses/TripResponse.java
+++ road-trip-api/src/main/java/road/trip/api/responses/TripResponse.java
@@ -1,10 +1,12 @@
 package road.trip.api.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NonNull;
 import org.springframework.web.bind.annotation.ResponseBody;
 import road.trip.persistence.models.Location;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
@@ -16,8 +18,8 @@ public class TripResponse {
     private Long id;
     @NonNull
     private String name;
-    @NonNull
-    private LocalDateTime startDate;
+    @NonNull @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate startDate;
     @NonNull
     private int duration;
     @NonNull
