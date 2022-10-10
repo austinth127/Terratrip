24724b1bd390b57094518e5b45acb162998f1604
PJWallace1 <90213252+PJWallace1@users.noreply.github.com>
2022-10-09 20:38:02 -0500

    GeoApify Client Implementation

---
+++ road-trip-api/src/main/java/road/trip/api/clients/geoapify/GeoApifyClient.java
@@ -0,0 +1,110 @@
+package road.trip.api.clients.geoapify;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import road.trip.persistence.models.Stop;
+import road.trip.persistence.models.Trip;
+
+import javax.net.ssl.SSLSession;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Optional;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class GeoApifyClient {
+
+    private static final String BASE_URL = "https://api.geoapify.com";
+
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final StopMapper stopMapper;
+
+    private URI buildUri(String path) {
+        try {
+            return new URIBuilder(BASE_URL)
+                .setPath(path)
+                .build();
+        } catch (URISyntaxException e) {
+            log.error(e);
+            return null;
+        }
+    }
+
+    private URI buildUri(String path, List<NameValuePair> queryParams) {
+        try {
+            return new URIBuilder(BASE_URL)
+                .setPath(path)
+                .addParameters(queryParams)
+                .build();
+        } catch (URISyntaxException e) {
+            log.error(e);
+            return null;
+        }
+    }
+
+    //TODO: test
+    public List<Stop> getStopByName(String name){
+        URI uri = buildUri("/v2/places?name=" + name + "&limit=20");
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+            .uri(uri)
+            .GET()
+            .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(httpRequest, BodyHandlers.ofString());
+
+            return stopMapper.getStopsFromJSON(httpResponse.body());
+        } catch (IOException | InterruptedException e) {
+            log.error(e);
+            return null;
+        }
+    }
+
+    //TODO: test
+    public List<Stop> getStopsByCoords(long lon, long lat, long radius){
+        URI uri = buildUri("/v2/places?filter=circle:" + lon + "," + lat + "," + radius + "&limit=20");
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(httpRequest, BodyHandlers.ofString());
+
+            return stopMapper.getStopsFromJSON(httpResponse.body());
+        } catch (IOException | InterruptedException e) {
+            log.error(e);
+            return null;
+        }
+    }
+
+    //TODO: test
+    public List<Stop> getRecommendedStops(long lon1, long lat1, long lon2, long lat2){
+        URI uri = buildUri("/v2/places?filter=rect:" + lon1 + "," + lat1 + "," + lon2 + "," + lat2 + "," + "&limit=20");
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(httpRequest, BodyHandlers.ofString());
+
+            return stopMapper.getStopsFromJSON(httpResponse.body());
+        } catch (IOException | InterruptedException e) {
+            log.error(e);
+            return null;
+        }
+    }
+}
--- road-trip-api/src/test/java/road/trip/api/clients/GeoApifyClientTester.java
+++ road-trip-api/src/test/java/road/trip/api/clients/GeoApifyClientTester.java
@@ -1,9 +1,14 @@
 package road.trip.api.clients;
 
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import road.trip.api.clients.geoapify.GeoApifyClient;
 
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class GeoApifyClientTester {
-    GeoApifyClient geoApifyClient = new GeoApifyClient();
+    GeoApifyClient geoApifyClient;
 
     @Test
     void testGetStopByName(){
