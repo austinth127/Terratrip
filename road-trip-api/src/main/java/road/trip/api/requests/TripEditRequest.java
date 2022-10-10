0a00ae1bb60f210c4d4a12d8ffde416ef12ae1b7
Ricardo Boone <Ricardo_boone1@baylor.edu>
2022-10-10 14:56:21 -0500

    TripEditRequest built

--- road-trip-api/src/main/java/road/trip/api/requests/TripEditRequest.java
+++ road-trip-api/src/main/java/road/trip/api/requests/TripEditRequest.java
@@ -1,4 +1,32 @@
 package road.trip.api.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
 public class TripEditRequest {
+
+    private String name;
+
+    @JsonProperty("advLevel")
+    private String adventureLevel;
+
+    @JsonProperty("duration")
+    private int driveDuration;
+
+    private double distance;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    private LocationRequest start;
+    private LocationRequest end;
+
+    private List<LocationRequest> stops;
 }
