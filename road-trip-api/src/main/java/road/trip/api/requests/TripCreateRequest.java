ea31dea512996141b6ed4c75ec58b5c69d290266
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2022-10-10 12:50:31 -0500

    Update TripCreateRequest.java

--- road-trip-api/src/main/java/road/trip/api/requests/TripCreateRequest.java
+++ road-trip-api/src/main/java/road/trip/api/requests/TripCreateRequest.java
@@ -1,9 +1,11 @@
 package road.trip.api.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,8 +20,10 @@ public class TripCreateRequest {
     private int duration;
     @NotNull
     private double distance;
-    @NotNull
-    private LocalDateTime startDate;
+    @NotNull   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate startDate;
+    @NotNull   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate endDate;
 //
 //    @NotNull
 //    private Location start;
