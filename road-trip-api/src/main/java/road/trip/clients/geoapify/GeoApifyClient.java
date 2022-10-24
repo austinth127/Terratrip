cc76ff48d233847b0d3a15520efced6e9fc5af2a
daniel-luper <jdaniel.luper@gmail.com>
2022-10-24 14:10:23 -0500

    geoapify client fixes

--- road-trip-api/src/main/java/road/trip/clients/geoapify/GeoApifyClient.java
+++ road-trip-api/src/main/java/road/trip/clients/geoapify/GeoApifyClient.java
@@ -6,6 +6,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import road.trip.persistence.models.Location;
 
@@ -23,6 +24,9 @@ import java.util.List;
 public class GeoApifyClient {
 
     private static final String BASE_URL = "https://api.geoapify.com";
+
+    @Value("${geoapify-key}")
+    private String API_KEY;
 
     private final HttpClient client = HttpClient.newHttpClient();
     private final LocationMapper locationMapper;
@@ -94,14 +98,12 @@ public class GeoApifyClient {
     }
 
     //https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=a9b12a2a2ae0491cb7874bbf0fab7115
-
-    //TODO: test
-    public List<Location> getRecommendedStops(double lon1, double lat1, double lon2, double lat2){
+    public List<Location> getRecommendedStops(double minLon, double minLat, double maxLon, double maxLat, List<String> categories) {
         URI uri = buildUri("/v2/places", List.of(
-            new BasicNameValuePair("categories", "activity"),
-            new BasicNameValuePair("filter", "rect:" + lon1 + "," + lat1 + "," + lon2 + "," + lat2),
+            new BasicNameValuePair("categories", String.join(",", categories)),
+            new BasicNameValuePair("filter", "rect:" + minLon + "," + minLat + "," + maxLon + "," + maxLat),
             new BasicNameValuePair("limit", "20"),
-            new BasicNameValuePair("apiKey", "a9b12a2a2ae0491cb7874bbf0fab7115")));
+            new BasicNameValuePair("apiKey", API_KEY)));
         try {
             String jsonBody = doGet(uri);
             return locationMapper.getStopsFromJSON(jsonBody);
