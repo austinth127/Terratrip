2a0af1cbf88aaa661ce5805e48bbaf4c02194207
daniel-luper <jdaniel.luper@gmail.com>
2022-11-29 21:51:17 -0600

    get rid of the random stuff

--- road-trip-api/src/test/java/road/trip/RoadTripContextTests.java
+++ road-trip-api/src/test/java/road/trip/RoadTripContextTests.java
@@ -4,51 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import road.trip.api.trip.TripController;
-import road.trip.api.trip.TripService;
-import road.trip.api.user.UserService;
-import road.trip.persistence.daos.TripRepository;
-import road.trip.persistence.daos.UserRepository;
-import road.trip.persistence.models.User;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 @Log4j2
 public class RoadTripContextTests {
-    @Autowired TripController tripController;
-    @Autowired TripService tripService;
-    @Autowired TripRepository tripRepository;
-    @Autowired UserRepository userRepository;
-
-    @MockBean
-    UserService userService;
-
-    @BeforeEach
-    void init() {
-        User user = userRepository.save(User.builder()
-            .name("Test User")
-            .emailAddress("test@test.com")
-            .username("tester")
-            .build());
-        when(userService.user()).thenReturn(user);
-    }
-
-    @AfterEach
-    void cleanUp() {
-        tripRepository.deleteAll();
-    }
-
     @Test
     void contextLoads() {
-        assertNotNull(tripController);
-        assertNotNull(tripService);
-        assertNotNull(tripRepository);
     }
 }
