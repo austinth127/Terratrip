c398aef410657d525cbace1347c8066ad82446f9
PJWallace1 <90213252+PJWallace1@users.noreply.github.com>
2022-12-05 13:34:53 -0600

    Modify refined route

--- road-trip-api/src/main/java/road/trip/util/UtilityFunctions.java
+++ road-trip-api/src/main/java/road/trip/util/UtilityFunctions.java
@@ -23,6 +23,8 @@ import java.lang.Math;
 
 public class UtilityFunctions {
 
+    final static Double METERS_IN_A_DEGREE = 111139.0;
+
     /**
      * Used to determine whether the time frame of a category overlaps with the time frame of a trip
      * @param categoryStartMonth
@@ -81,50 +83,52 @@ public class UtilityFunctions {
     }
 
     public static List<List<Double>> generateRefinedRoute(List<List<Double>> route, Double radius){
-        Double curDistance = radius * 2;
+        Double distanceToNextRefinedPoint = radius;
         List<List<Double>> refinedRoute = new ArrayList<>();
         //Add start and end to refined route
         refinedRoute.add(route.get(0));
         refinedRoute.add(route.get(route.size() - 1));
 
-        Double x1, x2, y1, y2, dist;
+        Double x1, x2, y1, y2, distanceBetweenPoints;
 
         //For each point except for the last one
         for(int i = 0; i < route.size() - 1; i++){
+            //Get the current point and the next one
             x1 = route.get(i).get(0);
             y1 = route.get(i).get(1);
             x2 = route.get(i+1).get(0);
             y2 = route.get(i+1).get(1);
-            dist = calcDist(x1, y1, x2, y2);
-            while(dist >= curDistance){
-                List<Double> newPoint = calcPointAlongLine(x1, y1, x2, y2, curDistance);
+            //Calculate the distance between the points
+            distanceBetweenPoints = calcDist(x1, y1, x2, y2);
+            while(distanceBetweenPoints >= distanceToNextRefinedPoint){
+                List<Double> newPoint = calcPointAlongLine(x1, y1, x2, y2, distanceToNextRefinedPoint);
                 refinedRoute.add(newPoint);
                 x1 = newPoint.get(0);
                 y1 = newPoint.get(1);
-                dist = calcDist(x1, y1, x2, y2);
+                distanceBetweenPoints = calcDist(x1, y1, x2, y2);
             }
-            curDistance-=dist;
+            distanceToNextRefinedPoint-=distanceBetweenPoints;
         }
 
         return refinedRoute;
     }
 
     private static Double calcDist(Double x1, Double y1, Double x2, Double y2){
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) * METERS_IN_A_DEGREE;
     }
 
-    private static List<Double> calcPointAlongLine(Double x1, Double y1, Double x2, Double y2, Double dist){
+    private static List<Double> calcPointAlongLine(Double x1, Double y1, Double x2, Double y2, Double distanceToNextRefinedPoint){
         Vector2D p1 = new Vector2D(x1, y1);
         Vector2D p2 = new Vector2D(x2, y2);
         Vector2D v = p2;
         v = v.subtract(p1);
-        System.out.println("Original V: " + v.getX() + " " + v.getY());
+        //System.out.println("Original V: " + v.getX() + " " + v.getY());
 
         v = v.normalize();
-        System.out.println("Normalize V: " + v.getX() + " " + v.getY());
+        //System.out.println("Normalize V: " + v.getX() + " " + v.getY());
 
-        v = v.scalarMultiply(dist);
-        System.out.println("Scaled V: " + v.getX() + " " + v.getY());
+        v = v.scalarMultiply(distanceToNextRefinedPoint / METERS_IN_A_DEGREE);
+        //System.out.println("Scaled V: " + v.getX() + " " + v.getY());
         v = v.add(p1);
         System.out.println("Final V: " + v.getX() + " " + v.getY());
 
